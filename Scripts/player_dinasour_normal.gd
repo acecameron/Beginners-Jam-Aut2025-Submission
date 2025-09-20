e87aa7b1a628aec54f2fcfa8b64b18b066fb1477
acecameron <acecmrn@gmail.com>
2025-09-20 22:52:49 +0300

    Add files via upload

---
+++ Scripts/player_dinasour_normal.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+signal game_over
+
+var speed := 150
+var alive := true
+
+func _ready():
+	velocity.x = speed
+
+func move():
+	move_and_slide()
+	$AnimatedSprite2D.play("running")
+	
+
+func _on_enemy_collison_area_area_entered(area: Area2D) -> void:
+	print("Cllided with an area")
+	if area.is_in_group("Obstacles"):
+		print("Stopping jow")
+		alive = false
+		$AnimatedSprite2D.play("death")
+		emit_signal("game_over")
+	
+func _physics_process(delta: float) -> void:
+	if alive:
+		move()
