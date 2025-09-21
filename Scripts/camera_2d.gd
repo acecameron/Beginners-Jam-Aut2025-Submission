357b2ba54728d3f688ee4f2fe9320d908a2a4c54
acecameron <acecmrn@gmail.com>
2025-09-21 17:30:13 +0300

    Delete Scripts/camera_2d.gd

--- Scripts/camera_2d.gd
+++
@@ -1,8 +0,0 @@
-extends Camera2D
-
-@onready var player = get_parent() # Adjust path as needed
-
-func _process(delta):
-	if player.is_on_floor():
-		global_position.y = 0
-		global_position = player.global_position
